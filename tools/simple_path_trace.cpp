ab856c0425222ab8c3063fb807d22b7a746e5bd4
Rich Wareham <rjw57@cam.ac.uk>
2012-02-14 18:25:15 +0000

    add some flesh to the simple_path_trace bones

--- tools/simple_path_trace.cpp
+++ tools/simple_path_trace.cpp
@@ -1,9 +1,15 @@
 #include <boost/foreach.hpp>
 #include <deque>
+#include <Eigen/Dense>
 #include <iterator>
 #include <io.hpp>
 #include <octree.hpp>
 #include <stdlib.h>
+#include <vector>
+
+typedef Eigen::Vector3f pixel_sample;
+typedef Eigen::aligned_allocator<Eigen::Vector3f> pixel_sample_allocator;
+typedef std::deque<pixel_sample, pixel_sample_allocator> pixel_sample_deque;
 
 int main(int argc, char** argv)
 {
@@ -13,14 +19,23 @@ int main(int argc, char** argv)
 		return EXIT_FAILURE;
 	}
 
+	// Load the octrees
 	std::deque<octree::crystalised_octree> trees;
 	io::load_crystal_octrees(argv[1], std::back_inserter(trees));
-
 	BOOST_FOREACH(const octree::crystalised_octree& tree, trees)
 	{
 		std::cout << "tree first loc: " << tree.extent_().loc << " + " << tree.extent_().size << '\n';
 	}
 
+	// Size of output
+	const int w(1280), h(720);
+
+	// Create a collection of pixel samples which is 3x(w*h)
+	Eigen::ArrayXXf samples(3, w*h);
+
+	float max_sample = samples.maxCoeff();
+
+	std::vector<data::pixel<uint8_t> > pixels(w*h, data::pixel<uint8_t>(0,0,0));
 
 	return EXIT_SUCCESS;
 }
